946a802b2de567935e88bb74aec11f5f4c4bb529
olena fedorenchyk <elenafedorenchyk@gmail.com>
2026-03-13 19:29:36 +0200

    homework2

---
+++ lessons/homework/hm2.py
@@ -0,0 +1,61 @@
+
+#task1
+# user_input = int(input("Enter a number: "))
+# print("Result:", user_input*user_input)
+# print("Result:", user_input**2)
+# print("Result:", pow(user_input,2))
+#
+# user_input = float(input("Enter a number: "))
+# print("Result:", user_input*user_input)
+# print("Result:", user_input**2)
+# print("Result:", pow(user_input,2))
+
+#task2
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+# number3 = int(input("Enter third number: "))
+# numbers_sum = number1 + number2 + number3
+# print(f"average of the numbers: {numbers_sum/3}")
+
+#task3
+# input_seconds = int(input('Enter minutes: '))
+# hours, seconds = divmod(input_seconds, 60)
+# print(f" {hours} hours, {seconds} seconds")
+
+#task4
+# price = int(input("Enter price: "))
+# sale = int(input("Enter sale: "))
+# print(f"final price: {price-price*sale/100}")
+
+#task5
+# user_input = int(input("Enter a number: "))
+# result=user_input % 10
+# print(f"last digit", result)
+
+#task6
+# first_str_cut_side= int(input("Enter first straight cut side: "))
+# second_str_cut_side = int(input("Enter second straight cut side: "))
+# print(f"rectangle dimension:",2 * first_str_cut_side + 2 * second_str_cut_side)
+
+#task7
+user_input = int(input("Enter a number: "))
+n4=user_input%10
+user_input=user_input //10
+n3=user_input%10
+user_input=user_input //10
+n2=user_input%10
+user_input=user_input //10
+n1=user_input%10
+user_input=user_input //10
+print(n1)
+print(n2)
+print(n3)
+print(n4)
+
+
+
+
+
+
+
+
