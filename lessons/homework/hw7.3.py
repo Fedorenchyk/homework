4b753be08c45321eac858a9d68b8b4f5f9dd60a3
olena fedorenchyk <elenafedorenchyk@gmail.com>
2026-03-29 21:40:13 +0300

    homework 7.3

--- lessons/homework/hw7.3.py
+++ lessons/homework/hw7.3.py
@@ -2,10 +2,9 @@ def second_index(text, some_str):
     f_index = text.find(some_str)
     s_index =text.find(some_str, f_index+1)
     if s_index <= 0:
-        text.find
         return None
     return s_index
-print(second_index('hi',  'h'))
+
 
 assert second_index("sims", "s") == 3, 'Test1'
 assert second_index("find the river", "e") == 12, 'Test2'
